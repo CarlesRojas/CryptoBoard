4fa68b5b7dbecdaadd15bff6e300f8ee8b444284
Carles <carlesrojas@outlook.com>
2021-05-30 11:49:12 +0200

    Change Color

--- src/contracts/CryptoBoard.sol
+++ src/contracts/CryptoBoard.sol
@@ -4,28 +4,100 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract CryptoBoard is ERC721 {
-    // General info
+
+    // ##################################################################
+    //    GLOBAR VARS
+    // ##################################################################
+
     uint256 public pixelCount;
-
-    // Mappings
-    mapping(uint256 => Pixel) public _pixels;
-
+    mapping(uint256 => Pixel) public pixels;
     address public owner = msg.sender;
 
+    // ##################################################################
+    //    STRUCTS
+    // ##################################################################
+
+    // Represents a pixel
     struct Pixel {
-        uint256 id;
+        uint256 coords;
         string color;
         address payable author;
         bool exists;
     }
 
+    // ##################################################################
+    //    MAIN FUNCTIONS
+    // ##################################################################
+
+    // Constructor fuunction
     constructor() ERC721("Pixel", "PXL") {}
 
+    // Create a new pixel
+    function mint(uint256 _coord, string memory _color) public onlyBy(owner) {
+        // Do not create if coordinate already exists
+        require(!pixels[_coord].exists);
+
+        // Only accept valid colors
+        require(isColor(_color));
+
+        // Increment the pixel count
+        pixelCount++;
+
+        // Mint pixel
+        _mint(msg.sender, _coord);
+
+        // Save the pixel
+        pixels[_coord] = Pixel(_coord, _color, payable(msg.sender), true);
+    }
+
+    // Change the color of a pixel
+    function changeColor(uint256 _coord, string memory _color) public {
+        // Do not change color if it does not exists
+        require(pixels[_coord].exists);
+
+        // Fetch the pixel
+        Pixel memory _pixel = pixels[_coord];
+
+        // Check that the owner is correct
+        require(_pixel.author == msg.sender);
+
+        // Only accept valid colors
+        require(isColor(_color));
+
+        // Check that the color is different than the old one
+        require(!stringsAreEqual(_color, _pixel.color));
+
+        // Change color
+        _pixel.color = _color;
+
+        // Save pixel
+        pixels[_coord] = _pixel;
+
+        // Emit event
+        emit PixelColorChanged(_pixel.coords, _pixel.color, _pixel.author, _pixel.exists);
+    }
+
+
+    // ##################################################################
+    //    MODIFIERS
+    // ##################################################################
+
+    // Only the owner of the contract can call functions with this modifier
     modifier onlyBy(address _account) {
         require(msg.sender == _account);
         _;
     }
 
+    // ##################################################################
+    //    AUX FUNCTIONS
+    // ##################################################################
+
+    // Returns true if the strings are equal
+    function stringsAreEqual(string memory s1, string memory s2) public pure returns(bool){
+        return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
+    }
+
+    // Returns true if the string is a valid HEX color code
     function isColor(string memory _color) public pure returns (bool) {
         // String to bytes
         bytes memory b = bytes(_color);
@@ -51,22 +123,15 @@ contract CryptoBoard is ERC721 {
         return true;
     }
 
-    function mint(uint256 _coord, string memory _color) public onlyBy(owner) {
-        // Do not create if coordinate already exists
-        require(!_pixels[_coord].exists);
+    // ##################################################################
+    //    EVENTS
+    // ##################################################################
 
-        // Only accept valid colors
-        require(isColor(_color));
-
-        // Increment the pixel count
-        pixelCount++;
-
-        // Mint pixel
-        _mint(msg.sender, _coord);
-
-        // Save the pixel
-        _pixels[_coord] = Pixel(_coord, _color, payable(msg.sender), true);
-    }
-
-    function changeColor(uint256 _coord, string memory _color) public {}
+    // Emited when the color of a pixel changes
+    event PixelColorChanged (
+        uint256 coords,
+        string color,
+        address payable author,
+        bool exists
+    );
 }
--- test/CryptoBoard.test.js
+++ test/CryptoBoard.test.js
@@ -4,7 +4,7 @@ const CryptoBoard = artifacts.require("./CryptoBoard.sol");
 require("chai").use(require("chai-as-promised")).should();
 
 // Testing
-contract("CryptoBoard", (accounts) => {
+contract("CryptoBoard", ([deployer, buyer]) => {
     let contract;
 
     before(async () => {
@@ -31,9 +31,66 @@ contract("CryptoBoard", (accounts) => {
         });
     });
 
-    describe("checking color", async () => {
-        it("checks that a string is a hex color code", async () => {
+    describe("pixels", async () => {
+        it("mints a new pixel", async () => {
+            const result = await contract.mint(0, "#000000");
+            const event = result.logs[0].args;
 
+            // SUCCESS
+            assert.equal(event.tokenId.toNumber(), 0, "id is correct");
+            assert.equal(event.from, "0x0000000000000000000000000000000000000000", "from is correct");
+            assert.equal(event.to, deployer, "to is correct");
+
+            // FAILURE
+            await contract.mint(0, "#000000").should.be.rejected;
+            await contract.mint(0, "#EC0583").should.be.rejected;
+            await contract.mint(1, "#EJ0583").should.be.rejected;
+            await contract.mint(1, "#E20583", { from: buyer }).should.be.rejected;
+        });
+
+        it("lists pixels", async () => {
+            const pixelCount = await contract.pixelCount();
+            const pixel = await contract.pixels(pixelCount.toNumber() - 1);
+
+            // SUCCESS
+            assert.equal(pixel.coords.toNumber(), pixelCount.toNumber() - 1, "coords are correct");
+            assert.equal(pixel.color, "#000000", "color is correct");
+            assert.equal(pixel.author, deployer, "author is correct");
+            assert.equal(pixel.exists, true, "exists is correct");
+        });
+
+        it("allows owners to change the color of a pixel", async () => {
+            const result = await contract.changeColor(0, "#FFFFFF");
+            const event = result.logs[0].args;
+
+            // SUCCESS
+            assert.equal(event.coords.toNumber(), 0, "coords are correct");
+            assert.equal(event.color, "#FFFFFF", "color is correct");
+            assert.equal(event.author, deployer, "author is correct");
+            assert.equal(event.exists, true, "exists is correct");
+
+            // FAILURE
+            await contract.changeColor(1, "#000000").should.be.rejected;
+            await contract.changeColor(0, "#000000", { from: buyer }).should.be.rejected;
+            await contract.changeColor(0, "#GGGGGG").should.be.rejected;
+            await contract.changeColor(0, "#FFFFFF").should.be.rejected;
+        });
+    });
+
+    describe("checking auxiliar functions", async () => {
+        it("checks that two strings are equal", async () => {
+            // SUCCESS
+            assert(await contract.stringsAreEqual("#35Aa44", "#35Aa44"));
+            assert(await contract.stringsAreEqual("testing", "testing"));
+            assert(await contract.stringsAreEqual("", ""));
+
+            // FAILURE
+            assert(!(await contract.stringsAreEqual("testing", "not testing")));
+            assert(!(await contract.stringsAreEqual("full", "")));
+            assert(!(await contract.stringsAreEqual("#3EAa44", "#3GAF44")));
+        });
+
+        it("checks that a string is a hex color code", async () => {
             // SUCCESS
             assert(await contract.isColor("#35Aa44"));
             assert(await contract.isColor("#F52344"));
@@ -49,22 +106,4 @@ contract("CryptoBoard", (accounts) => {
             assert(!(await contract.isColor("#3Aa44")));
         });
     });
-
-    describe("minting", async () => {
-        it("creates a new token", async () => {
-            const result = await contract.mint(0, "#EC058E");
-            const event = result.logs[0].args;
-
-            // SUCCESS
-            assert.equal(event.tokenId.toNumber(), 0, "id is correct");
-            assert.equal(event.from, "0x0000000000000000000000000000000000000000", "from is correct");
-            assert.equal(event.to, accounts[0], "to is correct");
-
-            // FAILURE: cannot mint same coords twice
-            await contract.mint(0, "#EC058E").should.be.rejected;
-            await contract.mint(0, "#EC0583").should.be.rejected;
-            await contract.mint(1, "#EJ0583").should.be.rejected;
-        });
-    });
-
 });
